d853220c897b9f459d66b7b6f1c83637797f8012
David Albert <developer@dtalbert.com>
2012-05-16 21:37:30 -0400

    added a call to curses.curs_set() to hide the cursor, which had been an ever-present grey rectangle following the snake around the screen

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main(scr):
 	curses.init_pair(1, curses.COLOR_RED, curses.COLOR_RED)
 	curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_GREEN)
 	curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_BLACK)
+	curses.curs_set(False)
 	scr.nodelay(True)
 
 	tickDuration = 0.25
