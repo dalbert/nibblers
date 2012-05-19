3556b0e76c398367bdc1e751e5880c9e46eaa71f
David Albert <developer@dtalbert.com>
2012-05-18 21:03:36 -0400

    snake of fixed length (5 segments) following user commands. The tail deletes as the head moves forward. coordinates for each segment are now kept in a list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import curses
 import time
+from collections import deque
 
 def main(scr):
 	status = curses.newwin(1, curses.COLS, 0, 0) 
@@ -7,24 +8,26 @@ def main(scr):
 	status.refresh()
 
 	curses.start_color()
-	curses.init_pair(1, curses.COLOR_RED, curses.COLOR_RED)
+	curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_RED)
 	curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_GREEN)
 	curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_BLACK)
 	curses.curs_set(False)
 	scr.nodelay(True)
 
-	tickDuration = 0.25
-	playerCoord = [29, 2]
+	tickDuration = 0.15
+	playerCoord = deque([[29, 2]])
 	keypress = None
 	currentDirection = curses.KEY_UP
 	nextDirection = curses.KEY_UP
 
 	draw(scr)
-	drawPlayer(scr, playerCoord)
+	drawPlayer(scr, playerCoord[0])
 	previousTime = time.time()
 	while True:
 		if getDeltaTime(previousTime) >= tickDuration:
-			playerCoord = movePlayer(scr, playerCoord, nextDirection)
+			playerCoord.append(movePlayer(scr, playerCoord[len(playerCoord)-1], nextDirection))
+			if len(playerCoord) > 5:
+				erasePlayer(scr, playerCoord.popleft())
 			previousTime = time.time()
 			currentDirection = nextDirection
 
@@ -32,6 +35,8 @@ def main(scr):
 
 		if keypress in (curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_RIGHT):
 			nextDirection = getDirection(currentDirection, keypress)
+		elif keypress == ord('p'):
+			curses.delay_output(5000)
 		elif keypress == ord('q'):
 			break
 		elif keypress == ord('r'):
@@ -47,17 +52,17 @@ def getDeltaTime(previousTime):
 	return time.time() - previousTime
 
 def movePlayer(scr, coord, direction):
-	erasePlayer(scr, coord)
+	newCoord = list(coord)
 	if direction == curses.KEY_UP:
-		coord[0] -= 1
+		newCoord[0] -= 1
 	elif direction == curses.KEY_DOWN:
-		coord[0] += 1
+		newCoord[0] += 1
 	elif direction == curses.KEY_LEFT:
-		coord[1] -= 2
+		newCoord[1] -= 2
 	elif direction == curses.KEY_RIGHT:
-		coord[1] += 2
+		newCoord[1] += 2
 	drawPlayer(scr, coord)
-	return coord
+	return newCoord
 
 def erasePlayer(scr, coord):
 	scr.addstr(coord[0], coord[1], ' ', curses.color_pair(3))
